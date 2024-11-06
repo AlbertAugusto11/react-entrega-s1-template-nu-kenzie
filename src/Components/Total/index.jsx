292f34a27925cc9b2976496f9f060d556579c103
Albert Augusto <167814598+AlbertAugusto11@users.noreply.github.com>
2024-11-05 21:56:35 -0300

    Update index.jsx

--- src/Components/Total/index.jsx
+++ src/Components/Total/index.jsx
@@ -4,7 +4,7 @@ export const Total = ({total}) => {
         <div className="total__Div">
             <div>
                 <h3>Valor total:</h3>
-                <h3 className="h3__total">R$ {total.toLocaleString("de-DE")}</h3>
+                <h3 className="h3__total">R$ {total.toLocaleString("de-DE")} reais</h3>
             </div>
             <p>O valor se refere ao saldo</p>
         </div>
